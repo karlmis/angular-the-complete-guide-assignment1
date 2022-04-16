c5d9830221fa6a7289abf8d990a4996e525c07b6
karlmis <karlmissiaen@gmail.com>
2022-04-16 12:30:14 +0200

    use selector as attribute in div instead of element

--- src/app/warning-alert/warning-alert.component.ts
+++ src/app/warning-alert/warning-alert.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 
 @Component({
-  selector: 'app-warning-alert',
+  selector: '[app-warning-alert]',
   templateUrl: 'warning-alert.component.html',
   styles: [`
     h3{
