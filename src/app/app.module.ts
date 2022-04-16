4b1e4b1cd3a395a651caa727f53e960fbca7d9fa
karlmis <karlmissiaen@gmail.com>
2022-04-16 12:20:01 +0200

    create first component manuall WarningAlert

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import {WarningalertComponent} from './warningalert/warningalert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WarningalertComponent
   ],
   imports: [
     BrowserModule,
